6ee3e012004ca557e82ebccf118c392ae5007f84
Walter Ferreira <69780634+FerreiraWalter@users.noreply.github.com>
2023-10-26 08:25:42 -0300

    wip

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"net/http"
+	"text/template"
+)
+
+type NewAggPage struct {
+	Title string
+	News  string
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	p := NewAggPage{Title: "Walter esteve aqui", News: "some news"}
+	t, _ := template.ParseFiles("basictemplating.html")
+	t.Execute(w, p)
+}
+
+func main() {
+	http.HandleFunc("/", indexHandler)
+	http.ListenAndServe(":8000", nil)
+}
