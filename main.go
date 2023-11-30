2b1ce8a4af2f75215b855fe36cdca795b2ea8b93
Walter Ferreira <69780634+FerreiraWalter@users.noreply.github.com>
2023-11-29 23:48:22 -0300

    wip

--- main.go
+++ main.go
@@ -18,5 +18,5 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.HandleFunc("/", indexHandler)
-	http.ListenAndServe(":80", nil)
+	http.ListenAndServe(":8000", nil)
 }
